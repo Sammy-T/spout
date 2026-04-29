d2983548f706896c967dedc9073ea5c5f38f3bf3
Sammy-T <cyam9019@gmail.com>
2026-04-29 04:41:04 -0400

    Set bg color

--- src/App.svelte
+++ src/App.svelte
@@ -1,89 +1,81 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from './assets/vite.svg'
-  import heroImg from './assets/hero.png'
-  import Counter from './lib/Counter.svelte'
-</script>
+<main>
+    <h1>bricks</h1>
 
-<section id="center">
-  <div class="hero">
-    <img src={heroImg} class="base" width="170" height="179" alt="" />
-    <img src={svelteLogo} class="framework" alt="Svelte logo" />
-    <img src={viteLogo} class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/App.svelte</code> and save to test <code>HMR</code></p>
-  </div>
-  <Counter />
-</section>
+    <hr>
 
-<div class="ticks"></div>
+    <section>
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+        <p>Lorem ipsum dolor sit amet vel dolore dolore aliquyam diam dolore nisl autem est accusam dignissim et eirmod.</p>
+    </section>
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true">
-      <use href="/icons.svg#documentation-icon"></use>
-    </svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank" rel="noreferrer">
-          <img class="logo" src={viteLogo} alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://svelte.dev/" target="_blank" rel="noreferrer">
-          <img class="button-icon" src={svelteLogo} alt="" />
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true">
-      <use href="/icons.svg#social-icon"></use>
-    </svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li>
-        <a href="https://github.com/vitejs/vite" target="_blank" rel="noreferrer">
-          <svg class="button-icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#github-icon"></use>
-          </svg>
-          GitHub
-        </a>
-      </li>
-      <li>
-        <a href="https://chat.vite.dev/" target="_blank" rel="noreferrer">
-          <svg class="button-icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#discord-icon"></use>
-          </svg>
-          Discord
-        </a>
-      </li>
-      <li>
-        <a href="https://x.com/vite_js" target="_blank" rel="noreferrer">
-          <svg class="button-icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#x-icon"></use>
-          </svg>
-          X.com
-        </a>
-      </li>
-      <li>
-        <a href="https://bsky.app/profile/vite.dev" target="_blank" rel="noreferrer">
-          <svg class="button-icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#bluesky-icon"></use>
-          </svg>
-          Bluesky
-        </a>
-      </li>
-    </ul>
-  </div>
-</section>
+    <hgroup>
+        <h2>Heading Group</h2>
+        <p>Lorem ipsum dolor sit amet feugait facilisi labore.</p>
+    </hgroup>
 
-<div class="ticks"></div>
-<section id="spacer"></section>
+    <section>
+        <p>
+            <abbr title="Abbreviation">Abbr.</abbr>
+            <code>&lt;abbr&gt;</code>
+        </p>
+        <p>
+            <mark>Highlight</mark>
+            <code>&lt;mark&gt;</code>
+        </p>
+        <p>
+            <strong>Bold</strong>
+            <code>&lt;strong&gt;</code>
+            <code>&lt;b&gt;</code>
+        </p>
+        <p>
+            <s>Strikethrough</s>
+            <code>&lt;s&gt;</code>
+        </p>
+        <p>
+            <i>Italic</i>
+            <code>&lt;i&gt;</code>
+            <code>&lt;em&gt;</code>
+            <code>&lt;cite&gt;</code>
+        </p>
+        <p>
+            <small>Small</small>
+            <code>&lt;small&gt;</code>
+        </p>
+        <p>
+            <del>Deleted</del>
+            <code>&lt;del&gt;</code>
+        </p>
+        <p>
+            Text<sub>Sub</sub>
+            <code>&lt;sub&gt;</code>
+        </p>
+        <p>
+            <ins>Inserted</ins>
+            <code>&lt;ins&gt;</code>
+        </p>
+        <p>
+            Text<sup>Sup</sup>
+            <code>&lt;sup&gt;</code>
+        </p>
+        <p>
+            <kbd>Ctrl + S</kbd>
+            <code>&lt;kbd&gt;</code>
+        </p>
+        <p>
+            <u>underline</u>
+            <code>&lt;u&gt;</code>
+        </p>
+    </section>
+
+    <blockquote>
+        "He was number 1."
+        <footer>
+            <cite>- Smitty Werbenjagermanjensen</cite>
+        </footer>
+    </blockquote>
+</main>
