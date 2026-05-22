390eff5356c90c3694ff20964811699578849ee6
Sammy-T <cyam9019@gmail.com>
2026-05-22 01:10:27 -0400

    Fix main-nav overlap

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <nav id="main-nav">
-    <a href="/"><h1>bricks</h1></a>
+    <a href="/"><strong>bricks</strong></a>
 
     <ul>
         <li><a href="#">GH</a></li>
@@ -8,6 +8,10 @@
 </nav>
 
 <main>
+    <h1>bricks</h1>
+
+    <hr/>
+
     <h2>Typography</h2>
     <section>
         <h1>Heading 1</h1>
@@ -16,7 +20,6 @@
         <h4>Heading 4</h4>
         <h5>Heading 5</h5>
         <h6>Heading 6</h6>
-        <hr/>
         <p>Lorem ipsum dolor sit amet vel dolore dolore aliquyam diam dolore nisl autem est accusam dignissim et eirmod.</p>
         <p>Lorem ipsum dolor sit amet lorem sanctus luptatum accusam dolor sea duis nonummy et amet at et duo vel kasd vero. 
             Amet in et no ipsum invidunt dolores et diam ipsum dignissim sadipscing. Invidunt amet ut elitr diam sanctus imperdiet imperdiet 
@@ -360,6 +363,7 @@
         margin: auto;
         position: sticky;
         top: 0;
+        z-index: 1;
     }
 
     main {
