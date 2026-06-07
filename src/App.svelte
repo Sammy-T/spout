ea31e4034dda06261c213d768250d6a04749f39c
Sammy-T <cyam9019@gmail.com>
2026-06-07 18:21:49 -0400

    Add description

--- src/App.svelte
+++ src/App.svelte
@@ -135,7 +135,10 @@
 
     <div class="contents">
         <h1 id="bricks">bricks</h1>
-        <!-- TODO: Desc -->
+        <p>A minimal, CSS-only library using reasonably supported, modern HTML and CSS features 
+            and constructed with preference for semantic HTML.</p>
+        <p>The goal of this library is to provide a simple CSS starter that can be used as-is or that 
+            can be customized and extended with your additional stylesheets.</p>
 
         <hr/>
 
